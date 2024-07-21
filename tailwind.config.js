22deb57c9f0072fb7e6c0750e6683b31d77e9e7e
tacowasabii <godseoril812@gmail.com>
2024-07-22 01:18:32 +0900

    chore: tailwind extend style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: '0.75rem',
+        sm: '0.875rem',
+        base: '1rem',
+        lg: '1.125rem',
+        xl: '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+      },
+      colors: {
+        mono: {
+          white: '#FFFFFF',
+          black: '#000000',
+        },
+      },
+      fontFamily: {
+        Pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
