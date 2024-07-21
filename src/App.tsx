b7d59b0c60d84021c6ada701486ed103febe8ff2
tacowasabii <godseoril812@gmail.com>
2024-07-22 01:18:45 +0900

    chore: font apply

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">hello world</div>;
+  return <div className="font-Pretendard text-3xl">hello world</div>;
 }
 
 export default App;
