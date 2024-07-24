4d1d2596e277c49117bd86fd169a51403b7381f8
June <frommillersplanet@gmail.com>
2024-07-24 14:07:13 +0900

    [Kan-64] Fix: 팝업 수정 (#12)
    * feat: 팝업
    
    * fix: storybook
    
    * style: 겹치는 코드
    
    * fix: popup
    
    * fix: onClose 삭제
    
    * fix: 스토리북 메타, eslint 에러

--- src/components/Popup/index.stories.tsx
+++ src/components/Popup/index.stories.tsx
@@ -1,35 +1,50 @@
-/* eslint-disable */
-
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
 import Popup, { PopupProps } from '.';
 
-export default {
+const meta: Meta<typeof Popup> = {
   title: 'Components/Popup',
   component: Popup,
-} as Meta;
+  render: (args: PopupProps) => {
+    const [isVisible, setIsVisible] = useState(true);
+    if (!isVisible) return <></>;
+    return (
+      <Popup
+        {...args}
+        onCancel={() => setIsVisible(false)}
+        onConfirm={() => setIsVisible(false)}
+      />
+    );
+  },
+};
+export default meta;
 
-const Template: StoryFn<PopupProps> = (args) => <Popup {...args} />;
+type Story = StoryObj<typeof Popup>;
 
-export const Default = Template.bind({});
-Default.args = {
-  message: `정말 나가시겠어요?\n작성된 내용이 모두 삭제됩니다.`,
+export const Default: Story = {
+  args: {
+    message: `정말 나가시겠어요?\n작성된 내용이 모두 삭제됩니다.`,
+  },
 };
 
-export const SingleButton = Template.bind({});
-SingleButton.args = {
-  message: '가입이 완료되었습니다!',
-  singleButton: true,
+export const SingleButton: Story = {
+  args: {
+    message: '가입이 완료되었습니다!',
+    singleButton: true,
+  },
 };
 
-export const CustomConfirmMessage = Template.bind({});
-CustomConfirmMessage.args = {
-  message: `'자바스크립트 기초'\n제목의 노트를 불러오시겠어요?`,
-  confirmMessage: '불러오기',
+export const ConfirmMessage: Story = {
+  args: {
+    message: `'자바스크립트 기초'\n제목의 노트를 불러오시겠어요?`,
+    confirmMessage: '불러오기',
+  },
 };
 
-export const FullExample = Template.bind({});
-FullExample.args = {
-  message: '전체 예제 팝업 메시지입니다.',
-  confirmMessage: '불러오기',
-  singleButton: false,
+export const Full: Story = {
+  args: {
+    message: '전체 예제 팝업 메시지입니다.',
+    confirmMessage: '불러오기',
+    singleButton: false,
+  },
 };
--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,12 +1,11 @@
 import { DeleteIcon } from '@assets';
-import { useState } from 'react';
 
 export interface PopupProps {
   message: string;
   confirmMessage?: string;
   singleButton?: boolean;
-  onConfirm?: () => void;
-  onCancel?: () => void;
+  onConfirm: () => void;
+  onCancel: () => void;
 }
 
 function Popup({
@@ -16,24 +15,6 @@ function Popup({
   onConfirm,
   onCancel,
 }: PopupProps) {
-  const [isVisible, setIsVisible] = useState(true);
-
-  const handleConfirm = () => {
-    if (onConfirm) {
-      onConfirm();
-    }
-    setIsVisible(false);
-  };
-
-  const handleCancel = () => {
-    if (onCancel) {
-      onCancel();
-    }
-    setIsVisible(false);
-  };
-
-  if (!isVisible) return null;
-
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="flex h-[203px] w-[300px] flex-col items-center justify-center gap-2.5 rounded-lg bg-white p-6 tablet:h-[216px] tablet:w-[450px]">
@@ -41,7 +22,7 @@ function Popup({
           <button
             type="button"
             className="flex items-center justify-center"
-            onClick={handleCancel}
+            onClick={onCancel}
             aria-label="Close"
           >
             <DeleteIcon width={24} height={24} />
@@ -52,16 +33,12 @@ function Popup({
             {message}
           </div>
         </div>
-        <div
-          className={`flex justify-center ${
-            singleButton ? 'tablet:justify-end' : 'gap-2'
-          } w-full`}
-        >
+        <div className="flex w-full justify-center gap-2">
           {!singleButton && (
             <button
               type="button"
               className="flex h-12 w-[120px] items-center justify-center rounded-xl border border-blue-500 bg-white text-base font-semibold leading-normal text-blue-500"
-              onClick={handleCancel}
+              onClick={onCancel}
               aria-label="Cancel"
             >
               취소
@@ -70,7 +47,7 @@ function Popup({
           <button
             type="button"
             className="flex h-12 w-[120px] items-center justify-center rounded-xl bg-blue-500 text-base font-semibold leading-normal text-white"
-            onClick={handleConfirm}
+            onClick={onConfirm}
             aria-label="Confirm"
           >
             <span className="block tablet:hidden">확인</span>
