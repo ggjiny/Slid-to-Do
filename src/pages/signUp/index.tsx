c30b06469fb295161a8be8c497a4b43efc5c75a8
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-24 18:15:10 +0900

    [KAN-27] Feature: 회원가입 페이지 퍼블리싱 (#15)
    * feat: 회원가입 페이지 퍼블리싱
    
    * fix: alias 수정

--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,5 +1,34 @@
+import { TextLogoIcon } from '@assets';
+import { Link } from 'react-router-dom';
+
 function SignUpPage() {
-  return <div>SignupPage</div>;
+  return (
+    <div className="mt-10 flex flex-col items-center tablet:mt-20 desktop:mt-40">
+      <div className="flex w-[340px] flex-col items-center tablet:w-[640px]">
+        <TextLogoIcon width={270} height={90} />
+        <div className="mt-4 self-start text-xl text-black">이름</div>
+        <div className="mt-4 h-10 w-full rounded-md bg-slate-200">.</div>
+        <div className="mt-4 self-start text-xl text-black">이메일</div>
+        <div className="mt-4 h-10 w-full rounded-md bg-slate-200">.</div>
+        <div className="mt-4 self-start text-xl text-black">비밀번호</div>
+        <div className="mt-4 h-10 w-full rounded-md bg-slate-200">.</div>
+        <div className="mt-4 self-start text-xl text-black">비밀번호 확인</div>
+        <div className="mt-4 h-10 w-full rounded-md bg-slate-200">.</div>
+        <button
+          type="button"
+          className="mt-12 h-10 w-full rounded-md bg-slate-400"
+        >
+          회원가입하기
+        </button>
+        <div className="mt-8 flex flex-row">
+          <span>이미 회원이신가요?</span>
+          <Link to="/signin" className="ml-2 text-blue-600 underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPage;
