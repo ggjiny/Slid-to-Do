6377424be13ffe1ee1e728b6dce79f2c60fe92a1
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-07-24 18:27:49 +0900

    [Kan-65] Feature: 사이드바 버튼 컴포넌트 적용 및 디자인 수정 (#16)
    * chore: plus 아이콘 추가
    
    * feat: 버튼 컴포넌트 적용 및 목표 추가 구현
    
    - 목표 추가는 데이터 보내기 전까지 구현.
    - 낙관적 업데이트는 미구현
    
    * chore: 아이콘 수정
    
    * style: 전반적인 디자인 수정
    
    * style: w-full로 변경
    
    * chore: 비활성화 플러스 아이콘 추가
    
    * style: 목표 입력 중 아이콘 색상 변경
    
    * fix: 테스트 코드 삭제
    
    * chore: 플러스 아이콘 수정
    
    * refactor: 플러스 아이콘 하나로 관리
    
    * style: 텍스트 스타일 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ export { ReactComponent as FlagIcon } from './flag.svg';
 export { ReactComponent as FoldIcon } from './fold.svg';
 export { ReactComponent as HomeIcon } from './home.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as PlusIcon } from './plus.svg';
 export { ReactComponent as ProfileIcon } from './profile.svg';
 export { ReactComponent as TextLogoIcon } from './textLogo.svg';
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -3,13 +3,16 @@ import {
   FoldIcon,
   HomeIcon,
   LogoIcon,
+  PlusIcon,
   ProfileIcon,
   TextLogoIcon,
 } from '@assets';
+import Button from '@components/Button';
 import { useState } from 'react';
 
 function SideBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const toggleSidebar = () => setIsOpen(!isOpen);
 
@@ -22,13 +25,13 @@ function SideBar() {
 
   return (
     <div
-      className={`absolute left-0 h-dvh w-60 transform p-4 ${isOpen ? 'translate-x-0' : '-translate-x-48'} bg-white transition-transform duration-300 ease-in-out`}
+      className={`absolute left-0 top-0 h-dvh w-[280px] transform px-6 py-4 ${isOpen ? 'translate-x-0' : '-translate-x-[220px]'} bg-white transition-transform duration-300 ease-in-out`}
     >
       <button
         type="button"
         aria-label="expend button"
         onClick={toggleSidebar}
-        className="fixed right-4 top-5 h-5 w-5"
+        className={`fixed ${isOpen ? 'right-6' : 'right-[18px]'} top-5 h-5 w-6`}
       >
         {!isOpen && <LogoIcon width={23} height={23} className="mb-4" />}
         <FoldIcon className={`${isOpen ? 'rotate-0' : 'rotate-180'}`} />
@@ -38,34 +41,71 @@ function SideBar() {
           <TextLogoIcon />
           <div className="mt-4 flex flex-row">
             <ProfileIcon width={64} height={64} />
-            <div className="ml-2">
-              <div className="h-4 text-sm">체다치즈</div>
-              <div className="h-4 text-sm">chedacheese@slid.kr</div>
-              <button type="button" className="mt-1">
-                <span className="text-xs">로그아웃</span>
+            <div className="ml-3 flex flex-col items-start justify-between">
+              <div className="h-4 text-sm font-semibold leading-5 text-slate-800">
+                체다치즈
+              </div>
+              <div className="h-4 text-sm font-medium leading-5 text-slate-600">
+                chedacheese@slid.kr
+              </div>
+              <button type="button">
+                <span className="text-xs font-normal leading-4 text-slate-400">
+                  로그아웃
+                </span>
               </button>
             </div>
           </div>
-          <div className="mb-4 mt-4">
-            <div className="h-10 w-full rounded-lg bg-blue-700"> </div>
+          <div className="my-6 flex justify-center">
+            <Button shape="solid" size="sm" additionalClass="w-full">
+              <PlusIcon className="mr-2 stroke-white" />
+              <span className="mr-2 text-base font-semibold">새 할 일</span>
+            </Button>
           </div>
-          <div className="absolute left-0 w-full border-b-2"> </div>
-          <div className="mb-2 mt-6 flex h-8 flex-row items-center">
-            <HomeIcon width={14} height={14} />
-            <div className="ml-4">대시보드</div>
+          <div className="absolute left-0 w-full border-b-[1px]"> </div>
+          <div className="my-4 mt-10 flex h-8 flex-row items-center">
+            <HomeIcon width={24} height={24} />
+            <div className="ml-2 text-lg font-medium">대시보드</div>
           </div>
-          <div className="absolute left-0 w-full border-b-2"> </div>
-          <div className="mb-2 mt-4 flex h-8 flex-row items-center">
-            <FlagIcon width={14} height={14} />
-            <div className="ml-4">목표</div>
+          <div className="absolute left-0 w-full border-b-[1px]"> </div>
+          <div className="my-4 mt-8 flex h-8 flex-row items-center">
+            <FlagIcon width={24} height={24} />
+            <div className="ml-2 text-lg font-medium">목표</div>
           </div>
           <ul>
             {mockGoalData.goals.map((item) => (
-              <li className="mt-2 text-sm">• {item.title}</li>
+              <li className="p-2 text-sm font-medium">• {item.title}</li>
             ))}
+            {isEditing && (
+              <li>
+                <span>• </span>
+                <input
+                  className="mt-4 h-8 w-[218px] rounded-md border border-gray-300 p-2 text-sm"
+                  placeholder="새 목표를 입력해주세요"
+                  onKeyDown={(event) => {
+                    // TODO: 엔터키 입력 시 목표 추가
+                    if (event.key === 'Enter') {
+                      setIsEditing(false);
+                    }
+                  }}
+                />
+              </li>
+            )}
           </ul>
-          <div className="mb-4 mt-4">
-            <div className="h-10 w-full rounded-lg bg-blue-700"> </div>
+
+          <div className="mt-6 flex justify-center">
+            <Button
+              shape="outlined"
+              size="sm"
+              additionalClass="w-full"
+              onClick={() => setIsEditing(true)}
+              disabled={isEditing}
+            >
+              <PlusIcon
+                className={`mr-2 ${isEditing ? 'stroke-slate-400' : 'stroke-blue-500'}`}
+              />
+
+              <span className="mr-2 text-base font-semibold">새 목표</span>
+            </Button>
           </div>
         </div>
       )}
