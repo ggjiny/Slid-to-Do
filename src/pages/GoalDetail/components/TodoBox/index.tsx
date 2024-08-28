86c4716a123a6bf0549675890c93125edb410961
tacowasabii <godseoril812@gmail.com>
2024-08-28 10:19:17 +0900

    fix: remove add todo in done box (#74)

--- src/pages/GoalDetail/components/TodoBox/index.tsx
+++ src/pages/GoalDetail/components/TodoBox/index.tsx
@@ -84,14 +84,20 @@ function TodoBox({
       >
         <div className="mb-4 flex items-center justify-between">
           <div className="text-lg font-bold leading-7">{title}</div>
-          <button
-            type="button"
-            onClick={() => setIsModalOpen(true)}
-            className="flex items-center text-sm font-semibold text-blue-500"
-          >
-            <PlusIcon width={16} height={16} className="mr-1 stroke-blue-500" />
-            <div className="leading-5">할일 추가</div>
-          </button>
+          {title === 'To do' && (
+            <button
+              type="button"
+              onClick={() => setIsModalOpen(true)}
+              className="flex items-center text-sm font-semibold text-blue-500"
+            >
+              <PlusIcon
+                width={16}
+                height={16}
+                className="mr-1 stroke-blue-500"
+              />
+              <div className="leading-5">할일 추가</div>
+            </button>
+          )}
         </div>
         {content}
       </div>
